4396e08520f1a6bc5d37bdf95155b679719b8296
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2024-09-09 08:01:38 -0400

    Remove lifecycle

--- module.tf
+++ module.tf
@@ -124,10 +124,4 @@ resource "azurerm_dns_txt_record" "azurerm_dns_txt_records" {
     }
   }
   tags                = merge(var.tags, try(each.value.tags, {}))
-
-  lifecycle {
-    ignore_changes = [
-      number_of_record_sets
-    ]
-  }
 }
